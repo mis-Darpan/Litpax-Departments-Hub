b249cfd63e27acc02f66e0954a47d6c92332a9d2
mis-Darpan <mis@litpaxtechnology.com>
2026-06-26 10:17:22 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbxpf5lSgKMKVKzPpdRAwbVsEHhf_il-9dzr7tBOoMsbvkIFFsF4z8DCStVIcbi60Ijs/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbyN3ptWGtg5LGx7Mdds4h6cwfdMDyC73DD174JWVmFgfA5XAdC6l9Qbq6hIIdv5dC3k/exec',
   CACHE_TTL: 5 * 60 * 1000,
 };
